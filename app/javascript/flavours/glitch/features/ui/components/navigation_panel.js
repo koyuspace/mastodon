8f7ce698425f0fb2b8e1a571ffe49df93338b372
koyu <me@koyu.space>
2020-10-31 09:37:54 +0100

    Update Discord link and mention chat.koyu.space

--- app/javascript/flavours/glitch/features/ui/components/navigation_panel.js
+++ app/javascript/flavours/glitch/features/ui/components/navigation_panel.js
@@ -43,7 +43,7 @@ const NavigationPanel = ({ onOpenSettings }) => (
 
     <hr />
 
-    <p><FormattedMessage id='navigation_bar.weekly' defaultMessage='Our Discord server is at 6MsakA3 and we host a big chat session every Saturday at 18:00 UTC.'></FormattedMessage></p>
+    <p><FormattedMessage id='navigation_bar.weekly' defaultMessage='Our Discord server is at 8gbd7YR and we host a big chat session at chat.koyu.space every Saturday at 18:00 UTC.'></FormattedMessage></p>
 
     {showTrends && <div className='flex-spacer' />}
     {showTrends && <TrendsContainer />}
