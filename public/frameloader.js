3e1f74354254c88711a5e083ca763680347fe728
koyu <me@koyu.space>
2021-02-18 09:55:50 +0100

    Update frameloader.js

--- public/frameloader.js
+++ public/frameloader.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
-    $.get("https://web.koyu.space/motd.html", function(data) {
-        $("#motd").html(data);
-    });
+	$.get("https://web.koyu.space/motd.html", function(data) {
+		$("#motd").html(data);
+	});
+	window.setInterval(function() {
+		$.get("https://web.koyu.space/motd.html", function(data) {
+			$("#motd").html(data);
+		});
+	}, 3000);
 });
