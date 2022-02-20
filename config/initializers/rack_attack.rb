9daa857338fc100d1491e20dd6f68b6688059556
koyu <me@koyu.space>
2022-02-20 09:37:21 +0100

    Revert sign up throttle changes, because nobody likes IPv6

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -82,13 +82,7 @@ class Rack::Attack
   end
 
   throttle('throttle_sign_up_attempts/ip', limit: 25, period: 5.minutes) do |req|
-    if req.post? && req.path == '/auth'
-      if req.remote_ip.ipv6?
-        req.remote_ip.mask(64)
-      else
-        req.remote_ip
-      end
-    end
+    req.remote_ip if req.post? && req.path == '/auth'
   end
 
   throttle('throttle_password_resets/ip', limit: 25, period: 5.minutes) do |req|
