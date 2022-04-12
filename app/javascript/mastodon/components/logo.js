d00c7da02869389fdca839bc6aa26cb3ba3b3a32
koyu <me@koyu.space>
2022-04-12 18:42:40 +0200

    Change up logo

--- app/javascript/mastodon/components/logo.js
+++ app/javascript/mastodon/components/logo.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const Logo = () => (
-  <svg viewBox='0 0 216.4144 232.00976' className='logo'>
-    <use xlinkHref='#mastodon-svg-logo' />
-  </svg>
+  <div />
 );
 
 export default Logo;
