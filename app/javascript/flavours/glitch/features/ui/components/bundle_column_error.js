2696adb78601d21bfc100dfb9afe31bcc8d9528e
koyu <me@koyu.space>
2023-01-16 18:45:00 +0100

    Update error image

--- app/javascript/flavours/glitch/features/ui/components/bundle_column_error.js
+++ app/javascript/flavours/glitch/features/ui/components/bundle_column_error.js
@@ -138,7 +138,7 @@ class BundleColumnError extends React.PureComponent {
     return (
       <Column bindToDocument={!multiColumn}>
         <div className='error-column'>
-          <GIF src='/oops.gif' staticSrc='/oops.png' className='error-column__image' />
+          <GIF src='/oops.png' staticSrc='/oops.png' className='error-column__image' />
 
           <div className='error-column__message'>
             <h1>{title}</h1>
--- app/javascript/mastodon/features/ui/components/bundle_column_error.js
+++ app/javascript/mastodon/features/ui/components/bundle_column_error.js
@@ -138,7 +138,7 @@ class BundleColumnError extends React.PureComponent {
     return (
       <Column bindToDocument={!multiColumn}>
         <div className='error-column'>
-          <GIF src='/oops.gif' staticSrc='/oops.png' className='error-column__image' />
+          <img src='/oops.png' className='error-column__image' role='presentation' />
 
           <div className='error-column__message'>
             <h1>{title}</h1>
